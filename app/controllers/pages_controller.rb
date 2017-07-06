9d4a11aab4b0d65adcbedf84a7fa5069fd8bd6db
Heber Sandoval <he3ers@gmail.com>
2017-07-06 15:57:14 -0400

    All page layouts are working and look good.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def blog
+  end
+
+  def post
+  end
+
+  def portfolio
+  end
+
+  def project
+  end
+
+  def signin
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get 'blog', to: 'pages#blog'
+  get 'post', to: 'pages#post'
+  get 'portfolio', to: 'pages#portfolio'
+  get 'project', to: 'pages#project'
+  get 'signin', to: 'pages#signin'
+  get 'about', to: 'pages#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
