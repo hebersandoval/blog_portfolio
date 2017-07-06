3640a03ee39326f01348f75407c937bc7502e239
Heber Sandoval <he3ers@gmail.com>
2017-07-06 16:47:06 -0400

    Add post model and controller. Also, added a slug but seems that is not working properly.

---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ApplicationRecord
+  before_create :set_slug
+
+  def to_param
+    "#{id}-#{slug}"
+  end
+
+  private
+
+  def set_slug
+    self.slug = title.parameterize
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'blog', to: 'pages#blog'
-  get 'post', to: 'pages#post'
   get 'portfolio', to: 'pages#portfolio'
   get 'project', to: 'pages#project'
   get 'signin', to: 'pages#signin'
   get 'about', to: 'pages#about'
+
+  resources :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
