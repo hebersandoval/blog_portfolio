65f01c35198c4b0fae4461ed567951cdb2a2a5c0
Heber Sandoval <he3ers@gmail.com>
2017-07-06 16:59:25 -0400

    Add new and create action to post controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,27 @@ class PostsController < ApplicationController
   end
 
   def show
-    # @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
   def set_post
     @post = Post.find(params[:id])
   end
