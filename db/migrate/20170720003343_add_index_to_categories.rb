6d08ded71ec5d419ecda9a7ed3e145edb846efca
Heber Sandoval <he3ers@gmail.com>
2017-07-19 20:37:00 -0400

    Add index to Categories table for slug attribute for performance.

---
+++ db/migrate/20170720003343_add_index_to_categories.rb
@@ -0,0 +1,5 @@
+class AddIndexToCategories < ActiveRecord::Migration[5.0]
+  def change
+    add_index :categories, :slug
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719212612) do
+ActiveRecord::Schema.define(version: 20170720003343) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "slug"
+    t.index ["slug"], name: "index_categories_on_slug"
   end
 
   create_table "post_categories", force: :cascade do |t|
