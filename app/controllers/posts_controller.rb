70abdcc9844278709b8134dee8a2de74c49e2025
Heber Sandoval <he3ers@gmail.com>
2017-07-28 19:00:28 -0400

    Add devise for authentication to user model and role for authorization.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update]
   before_action :set_all_categories, only: [:index, :show, :new, :edit]
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#home'
   get 'signin', to: 'pages#signin'
   get 'services', to: 'pages#services'
